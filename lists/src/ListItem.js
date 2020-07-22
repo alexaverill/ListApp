211a9646cab5e8377a4d5699ba5f1bba85051a83
Alex <alex.averill2013@gmail.com>
2020-07-21 19:02:43 -0600

    Fixing return value to display username of claimee

--- lists/src/ListItem.js
+++ lists/src/ListItem.js
@@ -7,15 +7,18 @@ import {getID} from './Session';
 class ListItem extends React.Component {
     constructor(props){
         super(props);
-        this.state={claimed:this.props.claimed,claimedBy:this.props.claimedBy}
+        this.state={claimed:this.props.claimed,claimedBy:this.props.claimedBy,claimedName:''}
         this.claim = this.claim.bind(this)
         this.unclaim = this.unclaim.bind(this);
     }
     componentDidMount(){
         console.log(this.props.claimedBy);
+        if(this.props.claimedBy !== null && this.props.claimedBy !== undefined){
         getUser(this.props.claimedBy).then(data=>{
             console.log(data);
-        })
+            this.setState({claimedName:data[0].username});
+        });
+    }
     }
     claim(event){
         console.log(event);
@@ -37,7 +40,7 @@ class ListItem extends React.Component {
             if( this.state.claimedBy == getID()){
                 button=<Button variant="outline-primary" className="claimBtn" onClick={this.unclaim}> UnClaim </Button>;
             }else if(this.state.claimed ){
-                button='Claimed By:'//<Button variant="outline-primary" className="claimBtn" disabled="true"> Claim </Button>;
+                button=`Claimed By:${this.state.claimedName}`//<Button variant="outline-primary" className="claimBtn" disabled="true"> Claim </Button>;
             }else{
                 button = <Button variant="primary" className="claimBtn" onClick={this.claim}> Claim </Button>
             }
